18aad6fe7ac26ef2577faf21241813e2954f92cd
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-12 12:29:37 +0600

    input error

---
+++ add-multiply.js
@@ -0,0 +1,75 @@
+// addition
+
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+const firsTtotal = add(62, 50);
+console.log('firsTtotal', firsTtotal);
+const secondTotal = add(30, 25);
+console.log('secondTotal', secondTotal);
+
+
+
+// subtraction
+
+
+function sub(num11, num22) {
+    const sum = num11 - num22;
+    return sum;
+}
+const FirsTtotal = sub(30, 15);
+console.log(FirsTtotal);
+const SecondTotal = sub(3, 2);
+console.log('SecondTotal', SecondTotal);
+
+
+
+// multiplication
+
+
+
+function mul(num13, num14) {
+    const sum = num13 * num14;
+    return sum;
+}
+const fIrsTtotal = mul(60, 52);
+console.log(fIrsTtotal);
+const secondTotaL = mul(30, 200);
+console.log(secondTotaL);
+
+
+
+// divition
+
+
+
+
+function div(num15, num20) {
+    const sum = num15 / num20;
+    return sum;
+}
+const fiRsTtotal = div(60, 50);
+console.log(fiRsTtotal);
+const secOndTotal = div(30, 20);
+console.log(secOndTotal);
+
+
+
+
+
+// percentage
+
+
+
+
+
+function per(num15, num20) {
+    const sum = num15 % num20;
+    return sum;
+}
+const fiRsTtotAl = per(76, 85);
+console.log(fiRsTtotal);
+const secOndTotAl = per(30, 20);
+console.log(secOndTotal);
--- reverse-letter.js
+++ reverse-letter.js
@@ -1,14 +1,24 @@
 function reversestring(text) {
+
     let reversee = '';
+
     for (const letter of text) {
+
         // console.log(letter);
+
         reversee = letter + reversee;
+
     }
+
 
     return reversee;
 }
 
 
 const greetings = 'helo how are you'
+
+
 const reversed = reversestring(greetings);
+
+
 console.log(reversed);
